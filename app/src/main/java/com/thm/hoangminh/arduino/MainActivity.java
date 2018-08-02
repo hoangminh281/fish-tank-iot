8054b6e0172fcb46b7eefc8679283a7642d66867
Tran Hoang Minh <tranhoangminhptit@gmail.com>
2018-08-02 16:01:10 +0700

    Updated android, arduino - Add new auto turn the light

--- app/src/main/java/com/thm/hoangminh/arduino/MainActivity.java
+++ app/src/main/java/com/thm/hoangminh/arduino/MainActivity.java
@@ -14,10 +14,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    TextView txtKhiga;
-    TextView txtNhietdo;
-    SeekBar sbThreshold;
-    TextView txtThreshold;
+    TextView txtHumi;
+    TextView txtEnvTemp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,59 +23,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        txtKhiga = findViewById(R.id.textViewKhiga);
-        txtNhietdo = findViewById(R.id.textViewNhietdo);
-        sbThreshold = findViewById(R.id.seekBarThreshold);
-        txtThreshold = findViewById(R.id.textViewThreshold);
+        txtHumi= findViewById(R.id.textViewHumi);
+        txtEnvTemp = findViewById(R.id.textViewEnvTemp);
 
-        // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference myRef = database.getReference();
 
-        // Read from the database
-        myRef.child("GASSENSOR_FIGURE").addValueEventListener(new ValueEventListener() {
+        myRef.child("humidity").addValueEventListener(new ValueEventListener() {
 
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue().toString();
-                txtKhiga.setText(value);
+                if (dataSnapshot.exists()) {
+                    double value = dataSnapshot.getValue(double.class);
+                    txtHumi.setText(value + "");
+                }
             }
 
             @Override
             public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Toast.makeText(MainActivity.this, "Failed to read value GAS.", Toast.LENGTH_SHORT).show();
             }
         });
-        myRef.child("TEMPSENSOR_FIGURE").addValueEventListener(new ValueEventListener() {
+        myRef.child("env_temperature").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String value = dataSnapshot.getValue().toString();
-                txtNhietdo.setText(value);
+                if (dataSnapshot.exists()) {
+                    double value = dataSnapshot.getValue(double.class);
+                    txtEnvTemp.setText(value + "");
+                }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                Toast.makeText(MainActivity.this, "Failed to read value TEMP.", Toast.LENGTH_SHORT).show();
-            }
-        });
-        sbThreshold.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                myRef.child("ThresHold").setValue(i);
-                txtThreshold.setText(String.valueOf(i));
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
             }
         });
     }
--- fish-tank-iot/arduino/Arduino.ino
+++ fish-tank-iot/arduino/Arduino.ino
@@ -3,31 +3,66 @@
 #include <DallasTemperature.h>
 #include <SoftwareSerial.h>
 
-const int DHT_PIN = 4;            //DHT11 is connected to pin D2
+const int DHT_PIN = 2;
 const int DHT_TYPE = DHT11;
 DHT dht(DHT_PIN, DHT_TYPE);
 
-#define ONE_WIRE_BUS 5
+#define ONE_WIRE_BUS 3
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature dltemp(&oneWire);
 
-SoftwareSerial mySerial(2, 3); //Rx = 2, Tx = 3
+#define led1 4
 
 double envtemp = 0.0f;
 double humi = 0.0f;
 double watertemp = 0.0f;
+String data;
+String key;
+String value;
+int currentTimeStamp;
+int led1TurnOnTimeStamp;
+int led1TurnOffTimeStamp;
 
 void setup()
 {
   Serial.begin(115200);
-  mySerial.begin(115200);
   dltemp.begin();
   dht.begin();
+  pinMode(led1, OUTPUT);
   delay(500);
 }
 
 void loop()
 {
+  data = receiveESP();
+  if (data!="")
+  {
+    key = data.substring(0,1);
+    value = data.substring(1);
+    if (key == "1")
+    {
+      currentTimeStamp = value.toInt();
+    }
+    else if (key == "2")
+    {
+      led1TurnOnTimeStamp = value.toInt();
+    }
+    else if (key == "3")
+    {
+      led1TurnOffTimeStamp = value.toInt();
+    }
+  }
+  if (currentTimeStamp!=0 && led1TurnOnTimeStamp!=0 && led1TurnOffTimeStamp!=0)
+  {
+    if (led1TurnOnTimeStamp <= currentTimeStamp && currentTimeStamp <= led1TurnOffTimeStamp)
+    {
+      digitalWrite(led1, HIGH);
+    }
+    else
+    {
+      digitalWrite(led1, LOW);
+    }
+  }
   envtemp = dht.readTemperature();
   humi = dht.readHumidity();
   dltemp.requestTemperatures();
@@ -39,19 +74,19 @@ void loop()
 
 void sendESP(String key, String value)
 {
-  mySerial.print(key + value);
+  Serial.print(key + value);
   delay(500);
 }
 
 String receiveESP()
 {
   String buff = "";
-  if (mySerial.available()) {
-    while (mySerial.available()) {
-      char data = (char)mySerial.read();
-      buff += data;
-    }
+  if (Serial.available())
+  {
+    while(Serial.available())
+    {
+      buff += (char)Serial.read();
+    }   
   }
   return buff;
 }
-
