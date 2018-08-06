6974d65b6eb6222f35d24f56c964d522f03d3962
hoangminh281 <tranhoangminhptit@gmail.com>
2018-08-06 23:13:46 +0700

    Updated new iot wifi uno

---
+++ fish-tank-iot/SSD1306 OLED 128X64/Oracle_VM_VirtualBox.lnk/Oracle_VM_VirtualBox.lnk.ino
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
+void setup(void) {
+  u8g2.begin();
+}
+
+void loop(void) {
+  u8g2.clearBuffer();					// clear the internal memory
+  u8g2.setFont(u8g2_font_unifont_t_symbols);
+  u8g2.drawUTF8(5, 20, "Snowman: ☃");  
+  u8g2.sendBuffer();					// transfer internal memory to the display
+  delay(1000);  
+}
+
