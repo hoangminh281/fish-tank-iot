a6ce451f228bf597b267ffa1f151afcf9d438854
Tran Hoang Minh <tranhoangminhptit@gmail.com>
2018-08-08 15:03:04 +0700

    Fixed arduino temperature

--- fish-tank-iot/Iot_wifi_uno/Iot_wifi_uno.ino
+++ fish-tank-iot/Iot_wifi_uno/Iot_wifi_uno.ino
@@ -15,25 +15,25 @@
 
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
 
-#define FIREBASE_HOST "qlcn-3d083.firebaseio.com"
-#define FIREBASE_AUTH "DqRKMGNqF5HjBQSVqDBN7FDFSA4OrcPBaGQL82RT"
-#define WIFI_SSID "TMA20years"
-#define WIFI_PASSWORD "TMA20years"
+#define FIREBASE_HOST             "qlcn-3d083.firebaseio.com"
+#define FIREBASE_AUTH             "DqRKMGNqF5HjBQSVqDBN7FDFSA4OrcPBaGQL82RT"
+#define WIFI_SSID                 "TMA20years"
+#define WIFI_PASSWORD             "TMA20years"
 
-#define KEY_HUMI "humidity"
-#define KEY_ENVTEMP "env_temperature"
-#define KEY_WATERTEMP "water_temperature"
-#define KEY_LED1TURNONTIMESTAMP "led1TurnOnTimeStamp"
-#define KEY_LED1TURNOFFTIMESTAMP "led1TurnOffTimeStamp"
+#define KEY_HUMI                  "humidity"
+#define KEY_ENVTEMP               "env_temperature"
+#define KEY_WATERTEMP             "water_temperature"
+#define KEY_LED1TURNONTIMESTAMP   "led1TurnOnTimeStamp"
+#define KEY_LED1TURNOFFTIMESTAMP  "led1TurnOffTimeStamp"
 
 NTPtime NTPch("ch.pool.ntp.org");
 strDateTime dateTime;
 
-const int DHT_PIN = 4;
+const int DHT_PIN = 16;
 const int DHT_TYPE = DHT11;
 DHT dht(DHT_PIN, DHT_TYPE);
 
-#define ONE_WIRE_BUS 5
+#define ONE_WIRE_BUS 02
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature dltemp(&oneWire);
 
@@ -41,7 +41,7 @@ double envtemp = 0.0f;
 double humi = 0.0f;
 double watertemp = 0.0f;
 
-#define led1 4
+#define led1 00
 
 int currentTimeStamp;
 int led1TurnOnTimeStamp;
@@ -50,9 +50,11 @@ int led1TurnOffTimeStamp;
 void setup() {
   Serial.begin(115200);
   dltemp.begin();
+  pinMode(2, INPUT_PULLUP);
   dht.begin();
   pinMode(led1, OUTPUT);
   u8g2.begin();
+  u8g2.enableUTF8Print();
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED)
@@ -68,7 +70,7 @@ void setup() {
   u8g2.setFont(u8g2_font_unifont_t_symbols);
   u8g2.setCursor(5, 20);
   u8g2.println("Connected to");
-  u8g2.setCursor(5, 29);
+  u8g2.setCursor(5, 39);
   u8g2.print(WIFI_SSID);
   u8g2.sendBuffer();
 
@@ -88,47 +90,7 @@ void loop() {
 
   if (dateTime.valid)
   {
-    
-    u8g2.clearBuffer();          // clear the internal memory
-    u8g2.setFont(u8g2_font_amstrad_cpc_extended_8f);  // choose a suitable font
-  
-    u8g2.setCursor(3,10);
-    u8g2.print ("Date : ");
-    u8g2.setCursor(55,10);
-    u8g2.print(dateTime.day, DEC);
-    u8g2.print('/');
-    u8g2.print(dateTime.month, DEC);
-    u8g2.print('/');
-    u8g2.print(dateTime.year, DEC);
-    
-    u8g2.setCursor(3,19);
-    u8g2.print ("Time : ");
-    u8g2.setCursor(62,19);
-    u8g2.print(dateTime.hour, DEC);
-    u8g2.print(':');
-    u8g2.print(dateTime.minute, DEC);
-    u8g2.print(':');
-    u8g2.print(dateTime.second, DEC);
-  
-    u8g2.setCursor(3,28);
-    u8g2.print ("env Temp :");
-    u8g2.print (envtemp, DEC);
-    u8g2.println ("°C");
-
-    u8g2.setCursor(3,37);
-    u8g2.print ("wat Temp :");
-    u8g2.print (watertemp, DEC);
-    u8g2.println ("°C");
-    
-    u8g2.setCursor (3, 46);
-    u8g2.print ("Humidity :");
-    u8g2.print (humi, DEC);
-    u8g2.println ('%');
-    
-    u8g2.setFont(u8g2_font_profont22_tf);
-    u8g2.drawFrame(0, 0, 128, 64);
-    u8g2.sendBuffer();          // transfer internal memory to the display
-
+    printScreen(dateTime, envtemp, humi, watertemp);
     if (led1TurnOnTimeStamp!=0 && led1TurnOffTimeStamp!=0)
     {
       currentTimeStamp = dateTime.hour * 10000 + dateTime.minute * 100 + dateTime.second;
@@ -159,3 +121,47 @@ int receiveIntFirebase(String key)
 {
   return Firebase.getInt(key);
 }
+
+void printScreen(strDateTime dateTime, double envtemp, double humi, double watertemp)
+{
+  u8g2.clearBuffer();          // clear the internal memory
+  u8g2.setFont(u8g2_font_amstrad_cpc_extended_8f);  // choose a suitable font
+
+  u8g2.setCursor(3,10);
+  u8g2.print ("Date : ");
+  u8g2.setCursor(55,10);
+  u8g2.print (dateTime.day, DEC);
+  u8g2.print ('/');
+  u8g2.print (dateTime.month, DEC);
+  u8g2.print ('/');
+  u8g2.print (dateTime.year, DEC);
+  
+  u8g2.setCursor(3,19);
+  u8g2.print ("Time : ");
+  u8g2.setCursor(62,19);
+  u8g2.print (dateTime.hour, DEC);
+  u8g2.print (':');
+  u8g2.print (dateTime.minute, DEC);
+  u8g2.print (':');
+  u8g2.print (dateTime.second, DEC);
+
+  u8g2.setCursor(3,28);
+  u8g2.print ("env T:");
+  u8g2.print ((int)envtemp, DEC);
+  u8g2.println ("°C");
+
+  u8g2.setCursor(3,37);
+  u8g2.print ("wat T:");
+  u8g2.print (watertemp, DEC);
+  u8g2.println ("°C");
+  
+  u8g2.setCursor (3, 46);
+  u8g2.print ("Humi :");
+  u8g2.print ((int)humi, DEC);
+  u8g2.println ('%');
+  
+  u8g2.setFont(u8g2_font_profont22_tf);
+  u8g2.drawFrame(0, 0, 128, 64);
+  u8g2.sendBuffer();          // transfer internal memory to the display
+}
+
